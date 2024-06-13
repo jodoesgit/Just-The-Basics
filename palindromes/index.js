ab093147778c59f8a1f89c4960b8d7b85cf59960
Jo <jojiboji@tuta.io>
2024-06-13 16:09:32 -0700

    Check value of user input if equal to palindrome

---
+++ palindromes/index.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("input")
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
+function check() {
+    const value = input.value
+    const reverse = reverseString(value)
+
+    if (value === reverse){
+        alert("This is a palindrome!")
+    } else {
+        alert("Not a palindrome")
+    }
+    
+    input.value = ""
+}
